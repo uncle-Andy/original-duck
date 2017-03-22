b5af93f71e75b84755f34b62e90ed3a769775dda
toast <623039975@qq.com>
2017-03-22 14:04:02 +0800

    增加用户自选股和删除功能

--- src/main/webapp/html/content/js/optional_stock_list.js
+++ src/main/webapp/html/content/js/optional_stock_list.js
@@ -11,13 +11,13 @@ function init() {
 
 function initData() {
     $.ajax({
-        type:'post',
-        url:"/Optional/get",
-        success:function (data){
+        type: 'post',
+        url: "/Optional/get",
+        success: function (data) {
             console.log('add');
             $('#optional_list').DataTable().rows.add(data).draw();
         },
-        error:function () {
+        error: function () {
             alert("请求失败");
         }
     });
@@ -25,13 +25,34 @@ function initData() {
 
 function initEvent() {
     // 初始化表格点击跳转事件
-    $('#optional_list tbody').on('click', 'tr', function () {
+    // 手动改造一下处理事件委托
+    $('#optional_list tbody').on('click', 'tr', function (e) {
+        let srcElem = e.srcElement || e.target;
+        if(srcElem.tagName == "BUTTON"){
+            // console.log($(this).find('td')[1].innerText);
+            let code = $(this).find('td')[1].innerText;
+            $.ajax({
+                url:'/Optional/del',
+                type:'post',
+                data:{code: code},
+                success: function(data){
+                    if(data) {
+                        window.location.reload();
+                    }
+                },
+                error: function(data){
+                    alert("ERROR");
+                }
+                
+            });
+            
+            return ;
+        }
         let table = $('#optional_list').DataTable();
         $('tr.selected').removeClass('selected');
         $(this).addClass('selected');
         let selected_row = table.row('.selected').index();
         let value = table.cell(selected_row, 1).data();
-
         window.location.href = "/html/content/html/stock_detail.html" + "?code=" + value;
     });
 }
@@ -53,33 +74,42 @@ function initPlugin() {
             {data: 'turnoverVol'},
             {data: 'changeRate'}
         ],
-        "aoColumnDefs": [{
-            "aTargets": [7],
-            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-                if (sData < "0") {
-                    ifHigher = 0;
-                    $(nTd).css('color', 'green')
-                } else {
-                    ifHigher = 1;
-                    $(nTd).css('color', 'red')
+        "aoColumnDefs": [
+            {
+                "aTargets": 7,
+                "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                    if (sData < "0") {
+                        ifHigher = 0;
+                        $(nTd).css('color', 'green')
+                    } else {
+                        ifHigher = 1;
+                        $(nTd).css('color', 'red')
+                    }
                 }
+            },
+            {
+                targets: 8,
+                render: function (data, type, row, meta) {
+                    return "<button class='btn btn-danger btn-xs' style='width: 100%' stock-code='" + row.code + "'>删除</button>";
+                }
+
             }
-        }],
+        ],
 //            "bProcessing":true,
 //            "iDisplayLength" : 40,
         "autoWidth": false
     });
     // 初始化高亮事件
     $('#optional_list tbody')
-        .on( 'mouseover', 'td', function () {
+        .on('mouseover', 'td', function () {
             console.log('over');
             let colIdx = optional_list.cell(this).index().row;
-            if ( colIdx !== null ) {
-                $( optional_list.rows().nodes() ).removeClass( 'highlight' );
-                $( optional_list.row( colIdx ).nodes() ).addClass( 'highlight' );
+            if (colIdx !== null) {
+                $(optional_list.rows().nodes()).removeClass('highlight');
+                $(optional_list.row(colIdx).nodes()).addClass('highlight');
             }
-        } )
-        .on( 'mouseleave', 'td', function () {
-            $( optional_list.rows().nodes() ).removeClass( 'highlight' );
-        } );
+        })
+        .on('mouseleave', 'td', function () {
+            $(optional_list.rows().nodes()).removeClass('highlight');
+        });
 }
